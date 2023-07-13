f7c332ec580ca14dd6acd8623be659d8f5be1e4d
Laurence Lagado <116421221+NoLongerLagado@users.noreply.github.com>
2023-07-13 21:01:37 +0800

    Fixing minor errors
    PS: There are still 2 errors in SqlNotif :(

---
+++ NotifInterface/DLL/SqlNotif.cs
@@ -0,0 +1,62 @@
+﻿using NotificationInterface;
+using System;
+using System.Data.SqlClient;
+using static NotificationInterface.DLL.InMemoryNotifData;
+
+namespace NotificationInterface.DLL
+{
+    public class SqlNotif
+    {
+        private readonly string _connectionString;
+
+        public SqlNotif(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public object SqlNotificationType { get; private set; }
+
+        public void ConnectAndListenForChanges()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            var command = new SqlCommand("SELECT StudentId, SenderName, ReceiverName, Content, TimeCode, IsRead FROM Notifications", connection);
+
+            var dependency = new SqlDependency(command);
+            dependency.OnChange += Dependency_OnChange;
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var notification = new Notification
+                    {
+                        StudentId = reader.GetInt32(0),
+                        senderName = NotificationRepository.GetUserByName(reader.GetString(1)),
+                        receiverName = UserRepository.GetUserByName(reader.GetString(2)),
+                        Content = reader.GetString(3),
+                        TimeCode = reader.GetDateTime(4),
+                        IsRead = reader.GetBoolean(5)
+                    };
+
+                   
+                }
+            }
+        }
+
+        private void Dependency_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            if (e.Type == SqlNotificationType.Change)
+            {
+                // Handle the change event
+            }
+
+            var dependency = (SqlDependency)sender;
+            dependency.OnChange -= Dependency_OnChange;
+            dependency.OnChange += Dependency_OnChange;
+
+            ConnectAndListenForChanges();
+        }
+    }
+}
---
+++ NotifInterface/NotifcationUI.cs
@@ -0,0 +1,88 @@
+﻿using NotificationInterface.BLL;
+using System;
+
+namespace InterfaceNotification
+{
+
+
+    public class Program
+    {
+        private static NotificationManagement _notificationManagement;
+      static void 
+
+        static void Main()
+        {
+            _notificationManagement = new NotificationManagement();
+
+            Console.WriteLine("Notification System!");
+
+            while (true)
+            {
+
+                if (NotificationManagement.NotificationMarketPlace(GetUserByName))
+                {
+                    Console.WriteLine($"Date : {DateTime.Now();}";
+                    Console.WriteLine(receiverName);
+                    Console.WriteLine("Enter notification content:");
+                    string content = Console.ReadLine();
+
+                }
+                else if (NotificationManagement.ReactPost(GetUserByName))
+                {
+                    Console.WriteLine($"Date : {DateTime.Now}";
+                    Console.WriteLine(senderName);
+                    Console.WriteLine("Enter notification content:");
+                    string content = Console.ReadLine();
+                    Console.WriteLine(receiver);
+                }
+                else if (NotificationManagement.NewPost(GetUserByName))
+                {
+                    Console.WriteLine($"Date : {DateTime.Now}";
+                    Console.WriteLine(sender);
+                    Console.WriteLine("Enter notification content:");
+                    string content = Console.ReadLine();
+                    Console.WriteLine(receiver);
+                }
+                else if (NotificationManagement.SharePost(GetUserByName))
+                {
+                    Console.WriteLine($"Date : {DateTime.Now}";
+                    Console.WriteLine(sender);
+                    Console.WriteLine("Enter notification content:");
+                    string content = Console.ReadLine();
+                    Console.WriteLine(receiver);
+                }
+                else if (NotificationManagement.Attendance(GetUserByName))
+                {
+                    Console.WriteLine($"Date : {DateTime.Now}";
+                    Console.WriteLine(sender);
+                    Console.WriteLine("Enter notification content:");
+                    string content = Console.ReadLine();
+                    Console.WriteLine(receiver);
+                }
+
+
+
+
+                /*Console.WriteLine("Enter sender name:");
+                string senderName = Console.ReadLine();
+
+                Console.WriteLine("Enter receiver name:");
+                string receiverName = Console.ReadLine();
+
+                Console.WriteLine("Enter notification content:");
+                string content = Console.ReadLine();*/
+
+                _notificationManagement.SendNotification(senderName, receiverName, content);
+
+                Console.WriteLine("Notification sent successfully!");
+                Console.WriteLine("Press Enter to send another notification or press any other key to exit.");
+
+                if (Console.ReadKey().Key != ConsoleKey.Enter)
+                    break;
+
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
